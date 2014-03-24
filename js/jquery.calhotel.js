1c283025b0a6848eab6cee186665ebbb476168ee
Francisco Carrillo <fraescaya10@gmail.com>
2014-03-24 17:22:10 -0500

    Posibilidad de enviar opciones de huespedes

--- js/jquery.calhotel.js
+++ js/jquery.calhotel.js
@@ -1,13 +1,37 @@
 //Plugin creacion calendario hotel
 (function ($, undefined) {
     //"use strict";
+    moment.lang('es');
+    c=0;
     //Configuraciones por defecto
     config_default = {
         num_fila: 5,
-        dias_sem: ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab','Dom']
+        dias_sem: ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab','Dom'],
+        datosroom: [
+            {
+                fecha_inicia: moment('2014-03-30'),
+                fecha_fin: moment('2014-03-30'),
+                numero_cuartos: 1,
+                cuartonro: 1,
+                nrocamas: 1,
+                valor: 1,
+                nombre_persona: 'Sr. Lopez',
+                color: 'blue'
+            },
+            {
+                fecha_inicia: moment('2014-03-25'),
+                fecha_fin: moment('2014-03-25'),
+                numero_cuartos: 1,
+                cuartonro: 3,
+                nrocamas: 1,
+                valor: 1,
+                nombre_persona: 'Sr. Perez',
+                color: 'red'
+            }
+        ]
+        
     };
-    moment.lang('es');
-    c=0;
+    
     
     $.fn.calhotel = function (config_usuario) {
         config_default = $.extend(config_default, config_usuario);
@@ -15,25 +39,30 @@
             var agenda, tbl, cont;
             agenda = new vistaAgenda(config_default);
             tbl = agenda.creaContenido();
+            
             //console.log(tbl);
             cont = $(this);
             cont.append(tbl);
+            ponerCuartosOcupados();
             $('#btnsig').click(function(e){
                 c++;
                 $('#titFecha').text(devuelveSemana(c));
                 actualizaCabeceras(primerdia(c));
+                ponerCuartosOcupados();
             });
             
             $('#btnant').click(function(e){
                 c--;
                 $('#titFecha').text(devuelveSemana(c));
                 actualizaCabeceras(primerdia(c));
+                ponerCuartosOcupados();
             });
             
             $('#btnhoy').click(function(e){
                 c=0;
                 $('#titFecha').text(devuelveSemana(c));
                 actualizaCabeceras(primerdia(c));
+                ponerCuartosOcupados();
             });
         });
     }
@@ -44,7 +73,7 @@
         function creaContenido() {
             var rangsem = devuelveSemana(c);
             var pd = primerdia(c).date();
-            var html = creaBotonesSup(rangsem) + creaTabla(pd);
+            var html = creaBotonesSup(rangsem) + creaTabla(pd);            
             return html;
         }
 
@@ -94,15 +123,15 @@
             var tbody = "<tbody class='cue-content'>";
             var f = '';
             for(var i=0 ; i <datos.num_fila;i++){
-                f+="<tr class='filatbl'> " +
+                f+="<tr class='filatbl' id='fila"+(i+1)+"'> " +
                 "<td class='celcab celroom'>Room "+(i+1)+"</td>" +
-                "<td class='celda' id='ct"+(i+1)+"0'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"1'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"2'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"3'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"4'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"5'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"6'></td>" +
+                "<td class='celda'><div class='evento' id='ct"+(i+1)+"0'><p></p></div></td>" +
+                "<td class='celda'><div class='evento' id='ct"+(i+1)+"1'><p></p></div></td>" +
+                "<td class='celda'><div class='evento' id='ct"+(i+1)+"2'><p></p></div></td>" +
+                "<td class='celda'><div class='evento' id='ct"+(i+1)+"3'><p></p></div></td>" +
+                "<td class='celda'><div class='evento' id='ct"+(i+1)+"4'><p></p></div></td>" +
+                "<td class='celda'><div class='evento' id='ct"+(i+1)+"5'><p></p></div></td>" +
+                "<td class='celda'><div class='evento' id='ct"+(i+1)+"6'><p></p></div></td>" +
                 "</tr>"
             }
             tbody += f + "</tbody>";
@@ -129,18 +158,27 @@
     function actualizaCabeceras(pd) {
         for(var i = 0; i<config_default.dias_sem.length;i++){
             var d= moment(pd).add('day',(i)).date();
-            //var d = moment(fc).date();
             $('.dia'+i).html(dias[i] +" "+d);
         }
     }
-    
-    
-    
-    //function buildEventContainer() {
-    //    daySegmentContainer =
-    //        $("<div class='fc-event-container' style='position:absolute;z-index:8;top:0;left:0'/>")
-    //            .appendTo(element);
-    //}
 
-    
+    function ponerCuartosOcupados() {
+        //if (test) {
+        //    //code
+        //}
+        
+        var daticos = config_default.datosroom;
+        for(d in daticos){
+            console.log(daticos[d].color);
+            var div = $('.cue-content').find('#ct'+daticos[d].cuartonro+moment(daticos[d].fecha_inicia).weekday());
+            div.css({
+                border: '1px solid green',
+                borderRadius: '5px',
+                boxShadow: '3px 3px 3px 3px rgba(0,0,0,0.3)',
+                backgroundColor: daticos[d].color===undefined ? 'cyan':daticos[d].color 
+            });
+            div.find('p').html('</br>'+daticos[d].nombre_persona);
+        }
+       
+    }
 })(jQuery);
--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
     $('.calhotel').calhotel({
-        num_fila: 8
+        num_fila: 50,
+        datosroom:[
+            {fecha_inicia: moment('2014-03-30'), //obligatorio
+                fecha_fin: moment('2014-03-30'), //obligatorio
+                cuartonro: 1,//obligatorio
+                nombre_persona: 'Sr. Lopez',//obligatorio
+            },
+            {fecha_inicia: moment('2014-03-26'), //obligatorio
+                fecha_fin: moment('2014-03-26'), //obligatorio
+                cuartonro: 3,//obligatorio
+                nombre_persona: 'Sr. Almeida',//obligatorio
+            },
+            {fecha_inicia: moment('2014-03-29'), //obligatorio
+                fecha_fin: moment('2014-03-29'), //obligatorio
+                cuartonro: 5,//obligatorio
+                nombre_persona: 'Sr. Vargas',//obligatorio
+                color:'red'
+            }
+            
+        ]
     });
 });
