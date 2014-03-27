02acd12febed33d93876da4af48165bbfea2cf58
Francisco Carrillo <fraescaya10@gmail.com>
2014-03-27 17:06:25 -0500

    Sombreado dia actual, vista semana

--- js/jquery.calhotel.js
+++ js/jquery.calhotel.js
@@ -29,9 +29,9 @@
             }
         ],
         haceClick: function(datos){}
-        
+
     };
-    
+
     //con esto se llama desde javascript
     $.fn.calhotel = function (config_usuario) {
         config_default = $.extend(config_default, config_usuario);
@@ -50,8 +50,8 @@
             desactivabotonHoy($('#btnhoy'));
             $('#btnSemana').attr('disabled',true);
             agenda.ponerCuartosOcupados();
-            
-            $('#btnsig').click(function(e){     
+
+            $('#btnsig').click(function(e){
                 if(vistadef==='semana'){
                     c++;
                     $('#titFecha').text(devuelveSemana(c));
@@ -63,12 +63,12 @@
                     $('#titFecha').text(diamost.format('MMMM D, YYYY'));
                     actualizaCabeceraDia(x);
                     dia.ponerCuartosOcupadosDia(diamost);
-                }      
+                }
                 desactivabotonHoy($('#btnhoy'));
             });
-            
-            $('#btnant').click(function(e){       
-                if(vistadef==='semana'){         
+
+            $('#btnant').click(function(e){
+                if(vistadef==='semana'){
                     c--;
                     $('#titFecha').text(devuelveSemana(c));
                     actualizaCabecerasSemana(primerdia(c));
@@ -79,10 +79,10 @@
                     $('#titFecha').text(diamost.format('MMMM D, YYYY'));
                     actualizaCabeceraDia(x);
                     dia.ponerCuartosOcupadosDia(diamost);
-                } 
+                }
                 desactivabotonHoy($('#btnhoy'));
             });
-            
+
             $('#btnhoy').click(function(e){
                 if(vistadef==='semana'){
                     c=0;
@@ -95,10 +95,10 @@
                     $('#titFecha').text(diamost.format('MMMM D, YYYY'));
                     actualizaCabeceraDia(x);
                     dia.ponerCuartosOcupadosDia(diamost);
-                } 
+                }
                 desactivabotonHoy($('#btnhoy'));
             });
-            
+
             $('#btnSemana').click(function(e){
                 c=0;//desactivar si no se quiere que coja la semana actual
                 $('#titFecha').text(devuelveSemana(c));
@@ -108,7 +108,7 @@
                 $('#btnSemana').attr('disabled',true);
                 $('#btnDia').attr('disabled',false);
             });
-            
+
             $('#btnDia').click(function(e){
                 x=0;//Desactivar si no se quiere q coja el dia actual
                 //console.log('Si llega a dia');
@@ -122,7 +122,7 @@
                 $('#btnSemana').attr('disabled',false);
                 $('#btnDia').attr('disabled',true);
             });
-            
+
             function poneSemana(opc){ //opc:1 si es llamada desde el btnSemana
                 if(opc === 1){
                    contenedor_secundario.find('.contenedor-tabla').remove();
@@ -130,7 +130,7 @@
                 tblContenidos= agenda.creaContenido();
                 contenedor_secundario.append(tblContenidos);
             }
-            
+
             function poneDia(){
                 contenedor_secundario.find('.contenedor-tabla').remove();
                 tblContenidos = dia.creaTabla();
@@ -138,7 +138,7 @@
             }
         });
     };
-    
+
     function desactivabotonHoy(btn) {
         var hoy = moment();
         if (mismaFecha(hoy,fechaInic)||mismaFecha(hoy,fechaFin)//si la fecha actual es igual a fecha inicio o fechafin
@@ -148,28 +148,28 @@
             $(btn).attr("disabled", false);
         }
     }
-    
+
     function creaBotonesSup() {
         var html = "<div class='botones-cabecera'><table class='tbl-botones'>" +
                 "<tr>" +
                 "<td id='btns_izq'>" +
-                "<button type='button' class='btn btn-default' id='btnant'><a>&laquo;</a></button>" +
-                "<button type='button' class='btn btn-default' id='btnhoy'><a>Hoy</a></button>" +
-                "<button type='button' class='btn btn-default' id='btnsig'><a>&raquo;</a></button>" +
+                "<button type='button' class='btn btn-default' id='btnant'><a style='text-decoration:none;'>&laquo;</a></button>" +
+                "<button type='button' class='btn btn-default' id='btnhoy'><a style='text-decoration:none;'>Hoy</a></button>" +
+                "<button type='button' class='btn btn-default' id='btnsig'><a style='text-decoration:none;'>&raquo;</a></button>" +
                 "</td>" +
                 "<td id='tit_tabla'>" +
                 "<h1 id='titFecha'>" + devuelveSemana(c)+ "</h1>" +
                 "</td>" +
                 "<td id='btns_der'>" +
-                "<button type='button' class='btn btn-default' id='btnSemana'><a>Semana</a></button>" +
-                "<button type='button' class='btn btn-default' id='btnDia'><a>Dia</a></button>" +
+                "<button type='button' class='btn btn-default' id='btnSemana'><a style='text-decoration:none;'>Semana</a></button>" +
+                "<button type='button' class='btn btn-default' id='btnDia'><a style='text-decoration:none;'>Dia</a></button>" +
                 "</td>" +
                 "</tr>" +
                 "</table></div>";
 
             return html;
     }
-    
+
     function devuelveSemana(incdec) {
         var fecha = moment().add('week', incdec);
         var iniSem = moment(fecha).startOf('week');//Inicia semana
@@ -182,19 +182,19 @@
             return iniSem.format('MMMM D')+' - '+finSem.format('MMMM D');
         }
     }
-    
+
     function devuelveDia(index){
         var fecha = moment().add('day',index);
         fechaInic = fecha;
         fechaFin = fecha;
         return fecha;
     }
-    
+
     function primerdia(incdec) {
         fecha = moment().add('week', incdec);
         return moment(fecha).startOf('week');
     }
-    
+
     function mismaFecha(f1,f2){
         var esIgual = false;
         if(moment(f1).isSame(f2,'year')&&//Cuando el diainicial es igual al dia mostrado
@@ -204,7 +204,7 @@
         }
         return esIgual;
     }
-    
+
     //pd= primer dia
     function actualizaCabecerasSemana(pd) {
         for(var i = 0; i<config_default.dias_sem.length;i++){
@@ -212,20 +212,20 @@
             $('.dia'+i).html(dias[i] +" "+d);
         }
     }
-    
+
     function actualizaCabeceraDia(d){
         $('.celcabdia').html(devuelveDia(x).format('dddd').toUpperCase());
     }
-    
+
     //**************************************** PARA LA VISTA DE AGENDA **********************************************
     function vistaAgenda (datos) {
         this.creaContenido = creaContenido;
         this.ponerCuartosOcupados = ponerCuartosOcupados;
-        
+
         function creaContenido() {
-            
+
             var pd = primerdia(c).date();
-            var html = creaTabla(pd);            
+            var html = creaTabla(pd);
             return html;
         }
 
@@ -257,22 +257,32 @@
         function creaCuerpoTbl() {
             var tbody = "<table><tbody>";
             var f = '';
+
             for(var i=0 ; i <datos.num_fila;i++){
                 f+="<tr class='filatbl' id='fila"+(i+1)+"'> " +
                 "<td class='celcab celroom'>Room "+(i+1)+"</td>" +
-                "<td class='celda' id='ct"+(i+1)+"0'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"1'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"2'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"3'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"4'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"5'></td>" +
-                "<td class='celda' id='ct"+(i+1)+"6'></td>" +
+                "<td class='celda"+eshoy(0)+"' id='ct"+(i+1)+"0'></td>" +
+                "<td class='celda"+eshoy(1)+"' id='ct"+(i+1)+"1'></td>" +
+                "<td class='celda"+eshoy(2)+"' id='ct"+(i+1)+"2'></td>" +
+                "<td class='celda"+eshoy(3)+"' id='ct"+(i+1)+"3'></td>" +
+                "<td class='celda"+eshoy(4)+"' id='ct"+(i+1)+"4'></td>" +
+                "<td class='celda"+eshoy(5)+"' id='ct"+(i+1)+"5'></td>" +
+                "<td class='celda"+eshoy(6)+"' id='ct"+(i+1)+"6'></td>" +
                 "</tr>";
             }
             tbody += f + "</tbody></table>";
             return tbody;
         }
-        
+
+        function eshoy(i){
+            var es = '';
+            var hoy = moment().weekday();
+            if(hoy===i){
+                es = ' celhoy';
+            }
+            return es;
+        }
+
         function ponerCuartosOcupados() {
             var daticos = datos.datosroom;
             for(d in daticos){
@@ -285,7 +295,7 @@
                         celda.append(creadiv(daticos[d]));
                         diactual = moment(diactual).add('day',1);
                         celda = $('.cont_tblcuerpo').find('#ct'+daticos[d].cuartonro+''+diactual.weekday());
-                    }                
+                    }
                 }else{
                     var diactual = moment(daticos[d].fecha_inicia);
                     var diafinocup = moment(daticos[d].fecha_fin);
@@ -293,14 +303,13 @@
                         remuevediv(celda);
                         diactual = moment(diactual).add('day',1);
                         celda = $('.cont_tblcuerpo').find('#ct'+daticos[d].cuartonro+''+diactual.weekday());
-                    } 
-                    
+                    }
+
                 }
             }
         }
     }
-    
-    
+
     function creadiv(datos) {
         var div = $("<div class='evento'><h5></h5><p></p></div>");
         div.css({
@@ -316,26 +325,25 @@
         .click(function(ev){
             config_default.haceClick.call(this,datos); //el this no se recibe como parametro pero se debe enviar, datos si es parametro q se recibe
         });
-        
+
         div.find('h5').html(moment(datos.fecha_inicia).hour()+':'+moment(datos.fecha_inicia).minute()+
                         ' - '+moment(datos.fecha_fin).hour()+':'+moment(datos.fecha_fin).minute());
         div.find('p').html(datos.nombre_persona);
         return div;
     }
-    
+
     function remuevediv(celda){
         celda.find('div').remove();
     }
-    
-    
+
+
     //**************************************** PARA LA VISTA DE DIA **********************************************
     function vistaDia (datos){
-        
+
         this.creaTabla= creaTabla;
         this.ponerCuartosOcupadosDia = ponerCuartosOcupadosDia;
-        
+
         function creaTabla() {
-            
             var tabla = "<div class='contenedor-tabla'>" +
             " <div class='cont_tblcabecera'>" +
             creaCabeceraTbl() +
@@ -370,13 +378,13 @@
             tbody += f + "</tbody></table>";
             return tbody;
         }
-        
+
         function ponerCuartosOcupadosDia(diamostrado){//objeto moment del dia mostrado
             //console.log(diamostrado.format('MMMM D'));
             var daticos = datos.datosroom;
             for (d in daticos) {
                  var celda = $('.cont_tblcuerpo').find('#ct'+daticos[d].cuartonro);
-                if(mismaFecha(daticos[d].fecha_inicia,diamostrado)){
+                if(mismaFecha(daticos[d].fecha_inicia,diamostrado)||mismaFecha(daticos[d].fecha_fin,diamostrado)){
                     remuevediv(celda);
                     celda.append(creadiv(daticos[d]));
                 }else if(moment(diamostrado).isBefore(daticos[d].fecha_fin)//Si diamostrado es antes de fechafin y diamostrado es mayor a fecha inicial
@@ -387,7 +395,7 @@
                     remuevediv(celda);
                 }
             }
-            
+
         }
-    }    
+    }
 })(jQuery);
